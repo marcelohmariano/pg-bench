9bc9b54ab08d75ea1475cee85af6b853c3fa0515
Marcelo Mariano <marcelo.hs.mariano@gmail.com>
2022-09-07 14:17:40 -0300

    Fix duplicated build step

--- hack/make/init.sh
+++ hack/make/init.sh
@@ -8,8 +8,6 @@ docker ps >/dev/null || exit 1
 for image in $(docker compose config --images); do
   [[ -n "$(docker images -q "$image")" ]] && continue
   docker compose build --build-arg UID="$(id -u)" --build-arg GID="$(id -g)"
-
-  [[ -n "$(docker images -q "$image")" ]] && continue
   docker compose pull
 done
 
